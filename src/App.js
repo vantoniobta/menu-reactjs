c5cd74016afa83f51134b331a81a1eeb500b6f99
vantonio <vale.residencia@gmail.com>
2018-05-24 18:58:45 -0500

    css reactjs

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 
 class App extends Component {
   render() {
+    let name      =  'vantonio'
+    let Textcolor =  '#F00B42'
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2 style={{color:Textcolor}}>Welcome {name}</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
